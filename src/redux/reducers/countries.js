9e00fe13aac90ba8675ad759894f828f0980c88c
Henry Lee <contact@henrylee.io>
2020-11-25 14:46:55 +1100

    setup initial store

---
+++ src/redux/reducers/countries.js
@@ -0,0 +1,10 @@
+const countries  = (defaultState = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return defaultState;
+    default:
+      return defaultState;
+  }
+}
+
+export default countries
