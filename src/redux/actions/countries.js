24ec9f239ba713cf9c325ee77774b77968c65636
Henry Lee <contact@henrylee.io>
2020-11-30 11:54:26 +1100

    update spacing link in action file

--- src/redux/actions/countries.js
+++ src/redux/actions/countries.js
@@ -1,15 +1,17 @@
-import axios from 'axios';
-import {UPDATE_COUNTRY_LIST, UPDATE_COUNTRY_LOADING_STATUS} from '../constants';
-
+import axios from "axios";
+import {
+  UPDATE_COUNTRY_LIST,
+  UPDATE_COUNTRY_LOADING_STATUS,
+} from "../constants";
 
 const updateLoadingStatus = (status) => {
   return {
     type: UPDATE_COUNTRY_LOADING_STATUS,
     payload: {
       status,
-    }
-  }
-}
+    },
+  };
+};
 
 const updateCountryList = (list) => {
   return {
@@ -17,23 +19,19 @@ const updateCountryList = (list) => {
     payload: {
       list,
     },
-  }
-}
-export const fetchCountryList = () => (
-  (dispatch, getState) => {
-    dispatch(updateLoadingStatus('LOADING'))
+  };
+};
 
-    axios.get('https://restcountries.eu/rest/v2')
-      .then(({data}) => {
-        dispatch(updateCountryList(data))
-        dispatch(updateLoadingStatus('SUCCESS'))
-      })
-      .catch((error) => {
-        dispatch(updateLoadingStatus('FAILED'))
-      })
+export const fetchCountryList = () => (dispatch, getState) => {
+  dispatch(updateLoadingStatus("LOADING"));
 
-  }
-)
-
-
-
+  axios
+    .get("https://restcountries.eu/rest/v2")
+    .then(({ data }) => {
+      dispatch(updateCountryList(data));
+      dispatch(updateLoadingStatus("SUCCESS"));
+    })
+    .catch((error) => {
+      dispatch(updateLoadingStatus("FAILED"));
+    });
+};
