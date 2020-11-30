188899a6531eed55186f52da1eb0f8bc48199e2c
Henry Lee <adria.0190@gmail.com>
2020-11-30 19:54:43 +1100

    clean up test file

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,17 +1,12 @@
 import { render, waitFor } from "@testing-library/react";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import { setupServer } from "msw/node";
-
 import { rest } from "msw";
 
-import App from "./App";
-
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-
 import reducers from "./redux/reducers";
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import App from "./App";
 
 describe("<App />", () => {
   const server = setupServer(
@@ -36,50 +31,48 @@ describe("<App />", () => {
   afterAll(() => server.close());
 
   it("render loading screen", async () => {
-    const store = createStore(
-      reducers,
-      composeEnhancers(applyMiddleware(thunk))
-    );
+    const store = createStore(reducers, applyMiddleware(thunk));
+
     const { getByTestId } = render(
       <Provider store={store}>
         <App />
       </Provider>
     );
+
     await waitFor(() => {
       expect(getByTestId("screen-loading")).not.toBeNull();
     });
   });
+
   it("render success screen", async () => {
-    const store = createStore(
-      reducers,
-      composeEnhancers(applyMiddleware(thunk))
-    );
+    const store = createStore(reducers, applyMiddleware(thunk));
+
     const { getByTestId, getAllByText } = render(
       <Provider store={store}>
         <App />
       </Provider>
     );
     await waitFor(() => getByTestId("screen-success"));
+
     expect(getAllByText("Country1").length).toBe(2);
     expect(getAllByText("Country2").length).toBe(2);
   });
 
   it("render failed screen", async () => {
-    const store = createStore(
-      reducers,
-      composeEnhancers(applyMiddleware(thunk))
-    );
     server.use(
       rest.get("https://restcountries.eu/rest/v2", (req, res, ctx) => {
         return res(ctx.status(500));
       })
     );
 
+    const store = createStore(reducers, applyMiddleware(thunk));
+
     const { getByTestId } = render(
       <Provider store={store}>
         <App />
       </Provider>
     );
+
     await waitFor(() => {
       expect(getByTestId("screen-failed")).not.toBeNull();
     });
